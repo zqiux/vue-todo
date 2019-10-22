6e259e818cc44285a8972606ecd94320b6fe3075
e <1342464229@qq.com>
2019-10-22 17:17:40 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-container>
+    <el-header>
+      <el-menu :default-active="defaultActiveIndex"
+      more="horizontal" style="display:flex;justify-content:center;align-items:center;">
+        <el-menu-item index="">Home</el-menu-item>
+        <el-menu-item index="/about">About</el-menu-item>
+      </el-menu>
+    </el-header>
+    <router-view :key="defaultActiveIndex"/>
+  </el-container>
+</template>
+
+<script>
+export default {
+  computed:{
+    defaultActiveIndex(){
+      return this.$route.path
+    }
+  }
+}
+</script>
+
